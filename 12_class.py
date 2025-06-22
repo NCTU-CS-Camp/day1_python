42808fd4a495c1c3d2c89c256fc8b904c69c7c7f
wwk0425 <180857742+wwk0425@users.noreply.github.com>
2025-06-22 23:55:23 +0800

    告訴我

---
+++ 12_class.py
@@ -0,0 +1,13 @@
+#TODO: 完成建立類別，並且創建新的寶可夢
+class new_pokemon:
+    def __init__(self, name, type, weak):
+        ???
+
+    def introduction(self):
+        ???
+    
+    def move(self, move_name):
+        ???
+
+# 建立新的寶可夢
+#TODO...
