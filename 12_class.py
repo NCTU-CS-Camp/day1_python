d9b6430fd558bb8a2709d79674b70128d46b36c8
wwk0425 <180857742+wwk0425@users.noreply.github.com>
2025-06-23 22:36:30 +0800

    update

--- 12_class.py
+++ 12_class.py
@@ -1,13 +1,23 @@
 #TODO: 完成建立類別，並且創建新的寶可夢
 class Pokemon:
     def __init__(self, name, type, weak):
-        ???
+        self.name = name
+        self.type = __(1)__
+        self.weak = __(2)__ 
 
     def introduction(self):
-        ???
+        print(f"__(3)__")
     
     def move(self, move_name):
-        ???
+        print(f"__(4)__")
 
 # 建立新的寶可夢
-???
+pikachu = Pokemon(__(5)__)
+pikachu.introduction()
+pikachu.move("耍憨")
+
+'''
+範例輸出:
+皮卡丘的屬性是電, 弱點是地面。
+皮卡丘使用了「耍憨」招式！
+'''
--- 13_json.py
+++ 13_json.py
@@ -1,8 +1,10 @@
 import json
 #TODO: 用load讀取錯誤的資料，並修改字典內容，再用dump轉成正確的json格式存成檔案
 jsonFile = open('wrong_pokedex.json','r', encoding='utf-8')
-data = ???
-???
+data = json.__(1)__(jsonFile)
+data['寶可夢資料']['妙蛙種子'][__(2)__] = __(2)__
+data['寶可夢資料']['小火龍'][__(3)__] = __(3)__
+data['寶可夢資料']['傑尼龜'][__(4)__] = __(4)__
 
 new_jsonFile = open('right_pokedex.json','w', encoding='utf-8')
-???
+json.__(5)__(data, new_jsonFile, ensure_ascii=False, indent=2)
