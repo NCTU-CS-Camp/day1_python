298da41a3cf6eb064178f6714f4e8a82bb3d72b7
ann <ann.cs12@nycu.edu.tw>
2025-06-23 10:59:15 +0800

    07_if_else.py modify

--- 07_if_else.py
+++ 07_if_else.py
@@ -7,11 +7,11 @@ hp = int(input("HP值："))
 print(f"\n{name} 的HP是 {hp}")
 
 # 判斷寶可夢所需治療
-if hp == 0: # 如果hp為0
+??? # 如果hp為0
     print("昏厥了！需要治療")
     
-elif hp < 50: # 如果hp<50
+??? hp < 50: # 如果hp<50
     print("受傷了，要休息")
     
-else: # 其它
+??? # 其它
     print("很健康！")
